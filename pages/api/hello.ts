95fcf534deff006f65ac725864e9ada7f73a9f92
mykola <mykola.kozubov@nearshorepartner.ch>
2020-05-14 22:38:52 +0300

    add linter config

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+//@ts-ignore
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
