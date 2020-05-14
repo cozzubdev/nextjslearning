f515ae1c9ca5a3da6a31bd073f4db9905d25f87c
mykola <mykola.kozubov@nearshorepartner.ch>
2020-05-14 23:57:31 +0300

    config for eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import React from 'react'
 
-export const Home = () => {
+export const Home = (): ReactElement => {
   return (
     <div className='container'>
       <Head>
